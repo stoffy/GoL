e53ec9c0cac9f9472cadefee3e40bb2536f8afd5
Christopher Lemke <chris.lemke@me.com>
2020-12-04 09:59:25 +0100

    Add helper

--- source/helpers.py
+++ source/helpers.py
@@ -3,13 +3,18 @@ import os
 
 
 SIZE = 100
-FRAMES = 10
+FRAMES = 100000
+
 DATA_FILE_PATH = os.path.abspath(
     __file__ + f'/../../data/game_of_life_{SIZE}-{FRAMES}.h5')
 H5PY_DATASET_NAME = 'gol_frames'
 
 
-f = h5py.File(DATA_FILE_PATH, 'r')
-dataset = f[H5PY_DATASET_NAME]
+def preview_dataset(frame):
+    file = h5py.File(DATA_FILE_PATH, 'r')
+    dataset = file[H5PY_DATASET_NAME]
+    print(f'\nShape: {dataset.shape}')
+    print(f'Preview for dataset (frame: {frame}):\n{dataset[frame]}')
 
-print(dataset[0])
+
+preview_dataset(0)
